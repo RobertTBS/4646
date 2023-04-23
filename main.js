157a1e0e34bb018250d7b78df970164fc16a30ae
Glitch (4646) <none>
2023-04-23 06:44:01 +0000

    Homepage

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
-
 document.getElementsByTagName("title")[0].innerHTML = "Wildcard 4646";
+if (location.href.substring(0,5)!="https"){window.location.href=window.location.href.replace('http', 'https');};
